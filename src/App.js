69cba4dcf387fe6279c93636e7498b024f33c155
Simon Wairiuko <37850208+Wairiuko@users.noreply.github.com>
2023-12-07 08:51:21 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import RegroTree from './regro.js';
+//import SymVal from './SymValFungible.js';
+import Symantra from './SymValMantra.js';
+
+function App() {
+  return (
+    <div className="App">
+     
+    <Symantra/>
+    
+    </div>
+  );
+}
+
+export default App;
