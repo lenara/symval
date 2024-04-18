b66914742cfb37cfb5b5d4055f836294a4262747
Simon Wairiuko <37850208+Wairiuko@users.noreply.github.com>
2024-04-18 11:39:52 +0300

    Add files via upload

---
+++ ccij/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          CCIJ token design
+        </p>
+        <a
+          className="App-link"
+          href="https://ccij.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CCIJ website
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
